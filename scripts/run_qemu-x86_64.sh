2f044d94cf633f786c199b4541bd412a83d0ab2e
hansung <hansung080@hanmail.net>
2020-05-26 20:07:45 +0900

    added vscode files and modified .gitignore

--- scripts/run_qemu-x86_64.sh
+++ scripts/run_qemu-x86_64.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-qemu-system-x86_64 -L . -m 64 -fda "/Users/hansung/Documents/work/ws/os/hans-os/hans-os.img" -hda "/Users/hansung/Documents/work/ws/os/hans-os/hdd.img" -boot a -localtime -M pc -serial tcp:127.0.0.1:7984,server,nowait -smp 2
+#!/bin/zsh
+#qemu-system-x86_64 -L . -m 64 -fda "/Users/hansung/work/ws/os/hans-os/hans-os.img" -hda "/Users/hansung/work/ws/os/hans-os/hdd.img" -boot a -localtime -M pc -serial tcp:127.0.0.1:7984,server,nowait -smp 2
+qemu-system-x86_64 -L . -m 64 -fda "/Users/hansung/work/ws/os/hans-os/hans-os.img" -hda "/Users/hansung/work/ws/os/hans-os/hdd.img" -boot a -M pc -serial tcp:127.0.0.1:7984,server,nowait -smp 2
